955d8fbe8c1ca5230d79ca28dd222cbf346c45cc
TheProgrammingMaestro <barca.rules.campeones@gmail.com>
2021-12-21 19:48:01 -0500

    venv setup

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame,sys
+pygame.init()
+
+WHITE = [255,255,255]
+BLACK = [0,0,0]
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 1000
+CIRCLE_RADIUS = 25
+
+screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
