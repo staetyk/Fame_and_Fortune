7f599a7382f56afd1855189a4b8072eed0f771a7
staetyk <bachaarondavis@gmail.com>
2021-12-24 14:04:27 -0500

    images

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ size = pygame.display.get_window_size()
 size = (size[0], size[1]-50)
 pygame.display.quit()
 pygame.display.init()
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+screen = pygame.display.set_mode(size)
 
 while True:
     events = pygame.event.get()
