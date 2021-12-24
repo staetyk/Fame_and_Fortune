6a4fb8236a8bbc1c9693b9f3ef0e5e75a00e684f
staetyk <bachaarondavis@gmail.com>
2021-12-24 16:51:00 -0500

    title

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 import pygame, sys
 
-from pygame.constants import K_END, QUIT, RESIZABLE
+from pygame.constants import K_END, K_RETURN, QUIT, RESIZABLE
 pygame.init()
 
 scren = pygame.display.set_mode((0, 0), RESIZABLE)
@@ -14,9 +14,30 @@ favi = pygame.image.load("images\Favicon.png")
 pygame.display.set_icon(favi)
 pygame.display.set_caption("Fame & Fortune", "F&F")
 
-while True:
+def end():
     events = pygame.event.get()
     if pygame.event.Event(QUIT) in events or pygame.event.Event(K_END) in events:
         pygame.display.quit()
         pygame.quit()
         quit
+
+def scale(img: pygame.Surface, width: int = None, hight: int = None) -> pygame.Surface:
+    size = img.get_size()
+    ratio = size[0] // size[1]
+    if width == None:
+        width = hight * ratio
+    elif hight == None:
+        hight = width // ratio
+    img = pygame.transform.scale(img, (width, hight))
+    return img
+
+screen.fill(0x171717)
+logo = pygame.image.load("images\LOGO.png")
+logo = scale(logo, width = size[0]-10)
+logo.set_colorkey((255,255,255))
+screen.blit(logo, (0,100))
+pygame.display.flip()
+
+
+while True:
+    end()
