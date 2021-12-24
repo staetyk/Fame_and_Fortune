fac066b90c37c296a6a5b327bf958c73832678b4
staetyk <bachaarondavis@gmail.com>
2021-12-24 14:27:00 -0500

    favion and caption

--- code/main.py
+++ code/main.py
@@ -1,14 +1,18 @@
 import pygame, sys
 
-from pygame.constants import K_END, QUIT
+from pygame.constants import K_END, QUIT, RESIZABLE
 pygame.init()
 
-scren = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+scren = pygame.display.set_mode((0, 0), RESIZABLE)
 size = pygame.display.get_window_size()
 size = (size[0], size[1]-50)
 pygame.display.quit()
 pygame.display.init()
 screen = pygame.display.set_mode(size)
+
+favi = pygame.image.load("images\Favicon.png")
+pygame.display.set_icon(favi)
+pygame.display.set_caption("Fame & Fortune", "F&F")
 
 while True:
     events = pygame.event.get()
