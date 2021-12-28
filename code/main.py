9c735add8f137e5ba7576c27b332d4291eb62714
TheProgrammingMaestro <barca.rules.campeones@gmail.com>
2021-12-27 20:02:02 -0500

    changed sizing method and fixed some errors

--- code/main.py
+++ code/main.py
@@ -3,23 +3,22 @@ import pygame, sys
 from pygame.constants import K_END, K_RETURN, QUIT, RESIZABLE
 pygame.init()
 
-scren = pygame.display.set_mode((0, 0), RESIZABLE)
-size = pygame.display.get_window_size()
-size = (size[0], size[1]-50)
-pygame.display.quit()
+monitor = pygame.display.Info()
 pygame.display.init()
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode((monitor.current_w, monitor.current_h -50), RESIZABLE)
+size = pygame.display.get_surface().get_size()
 
-favi = pygame.image.load("images/Favicon.png")
+favi = pygame.image.load(".venv\images\Favicon.png")
 pygame.display.set_icon(favi)
 pygame.display.set_caption("Fame & Fortune", "F&F")
 
 def end():
     events = pygame.event.get()
-    if pygame.event.Event(QUIT) in events or pygame.event.Event(K_END) in events:
-        pygame.display.quit()
-        pygame.quit()
-        quit
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+            pygame.quit()
+            quit
 
 def scale(img: pygame.Surface, width: int = None, hight: int = None) -> pygame.Surface:
     size = img.get_size()
@@ -32,7 +31,7 @@ def scale(img: pygame.Surface, width: int = None, hight: int = None) -> pygame.S
     return img
 
 screen.fill(0x171717)
-logo = pygame.image.load("images/LOGO.png")
+logo = pygame.image.load(".venv\images\LOGO.png")
 logo = scale(logo, width = size[0]-10)
 logo.set_colorkey((255,255,255))
 screen.blit(logo, (0,100))
