55c797626f067ed94ae7bdb0fd2190a2dd6b992e
Bach Davis <bachaarondavis@gmail.com>
2022-01-17 14:35:21 -0500

    updated enemy class

--- code/enemy.py
+++ code/enemy.py
@@ -1,13 +1,25 @@
 import pygame
 
 
+'''
+stat: Max HP, HP Regen, Max SP, SP Regen, Damage, Attack Speed, Critical Hit, Effects Given, Armor, Accuracy, Vunerability, Buffs, Debuffs, Medicine, Sickness, XP, Position, Type
+'''
+
+
 class enemy:
-    def __init__(self, stats: dict, texture: pygame.surface.Surface, dialogue: list[str] = []):
-        self.position = stats["Position"]
-        self.type = stats["Type"]
-        stats.pop("Position")
-        stats.pop("Type")
-        self.stats = stats
+    def __init__(self, stat: dict, texture: pygame.surface.Surface, dialogue: list[str] = []):
+        self.position = stat["Position"]
+        self.type = stat["Type"]
+        stat.pop("Position")
+        stat.pop("Type")
+
+        stat.update({
+            "HP" : stat["Max HP"],
+            "SP" : stat["Max SP"],
+            "Combo" : 0
+        })
+
+        self.stats = stat
         self.texture = pygame.transform.scale(texture, (100,100))
         self.dialogue = dialogue
         self.alive = True
